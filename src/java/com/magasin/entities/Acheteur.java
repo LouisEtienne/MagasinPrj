782e985600ed36da3a85db0434fc6cf4916c108b
usager <usager@B414E23.int.crosemont.qc.ca>
2015-10-07 12:30:07 -0400

    1er commit 12h29 7 oct 2015

---
+++ src/java/com/magasin/entities/Acheteur.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.magasin.entities;
+
+/**
+ *
+ * @author Louis-E
+ */
+public class Acheteur {
+    
+    private String courriel, motPasseAcheteur, noCarteCredit, nomAcheteur, prenomAcheteur,
+                    adresse, ville, province, codePostal, telephonePrincipal, telephoneSecondaire;
+    private boolean compteActif;
+
+    public Acheteur(String courriel, String motPasseAcheteur) {
+        this.courriel = courriel;
+        this.motPasseAcheteur = motPasseAcheteur;
+    }
+    
+    public Acheteur() {
+        this.courriel = "";
+        this.motPasseAcheteur = "";
+        this.noCarteCredit = "";
+        this.nomAcheteur = "";
+        this.prenomAcheteur = "";
+        this.adresse = "";
+        this.ville = "";
+        this.province = "";
+        this.codePostal = "";
+        this.telephonePrincipal = "";
+        this.telephoneSecondaire = "";
+        this.compteActif = true;
+        
+    }
+
+    public Acheteur(String courriel, String motPasseAcheteur, String noCarteCredit, String nomAcheteur, String prenomAcheteur,
+                    String adresse, String ville, String province, String codePostal, String telephonePrincipal, String telephoneSecondaire,
+                    boolean compteActif) {
+        this.courriel = courriel;
+        this.motPasseAcheteur = motPasseAcheteur;
+        this.noCarteCredit = noCarteCredit;
+        this.nomAcheteur = nomAcheteur;
+        this.prenomAcheteur = prenomAcheteur;
+        this.adresse = adresse;
+        this.ville = ville;
+        this.province = province;
+        this.codePostal = codePostal;
+        this.telephonePrincipal = telephonePrincipal;
+        this.telephoneSecondaire = telephoneSecondaire;
+        this.compteActif = compteActif;
+    }    
+
+    public String getCourriel() {
+        return courriel;
+    }
+
+    public void setCourriel(String courriel) {
+        this.courriel = courriel;
+    }
+
+    public String getMotPasseAcheteur() {
+        return motPasseAcheteur;
+    }
+
+    public void setMotPasseAcheteur(String motPasseAcheteur) {
+        this.motPasseAcheteur = motPasseAcheteur;
+    }
+
+    public String getNoCarteCredit() {
+        return noCarteCredit;
+    }
+
+    public void setNoCarteCredit(String noCarteCredit) {
+        this.noCarteCredit = noCarteCredit;
+    }
+
+    public String getNomAcheteur() {
+        return nomAcheteur;
+    }
+
+    public void setNomAcheteur(String nomAcheteur) {
+        this.nomAcheteur = nomAcheteur;
+    }
+
+    public String getPrenomAcheteur() {
+        return prenomAcheteur;
+    }
+
+    public void setPrenomAcheteur(String prenomAcheteur) {
+        this.prenomAcheteur = prenomAcheteur;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getTelephonePrincipal() {
+        return telephonePrincipal;
+    }
+
+    public void setTelephonePrincipal(String telephonePrincipal) {
+        this.telephonePrincipal = telephonePrincipal;
+    }
+
+    public String getTelephoneSecondaire() {
+        return telephoneSecondaire;
+    }
+
+    public void setTelephoneSecondaire(String telephoneSecondaire) {
+        this.telephoneSecondaire = telephoneSecondaire;
+    }
+
+    public boolean isCompteActif() {
+        return compteActif;
+    }
+
+    public void setCompteActif(boolean compteActif) {
+        this.compteActif = compteActif;
+    }
+
+    
+}
+
---
+++ src/java/com/magasin/jdbc/dao/implementation/AdministrateurDao.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor...
+ */
+package com.magasin.jdbc.dao.implementation;
+
+import com.magasin.entities.Acheteur;
+import com.magasin.entities.Administrateur;
+import com.magasin.jdbc.dao.Dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Louis
+ */
+public class AdministrateurDao extends Dao<Administrateur> {
+    //private Object cnx;
+
+    public AdministrateurDao(Connection c) {
+        super(c);
+    }
+    
+    @Override
+    public boolean create(Administrateur x) {
+        // TODO Auto-generated method stub
+
+        String req = "INSERT INTO user (`numId` , `mdp`) "
+                + "VALUES ('" + x.getCodeAdministrateur() + "','" + x.getMotPasseAdministrateur() + "','" + x.getNomAdministrateur() + "','" + x.getPrenomAdministrateur() + "')";
+
+        //System.out.println("REQUETE "+req);
+
+        Statement stm = null;
+        try {
+            stm = cnx.createStatement();
+            int n = stm.executeUpdate(req);
+            if (n > 0) {
+                stm.close();
+                return true;
+            }
+        } 
+        catch (SQLException exp) {
+        } finally {
+            if (stm != null) {
+                try {
+                    stm.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean delete(Administrateur x) {
+        // TODO Auto-generated method stub
+        Statement stm = null;
+        try {
+            stm = cnx.createStatement();
+            int n = stm.executeUpdate("DELETE FROM user WHERE codeAdministrateur='" + x.getCodeAdministrateur() + "'");
+            if (n > 0) {
+                stm.close();
+                return true;
+            }
+        } catch (SQLException exp) {
+        } finally {
+            if (stm != null) {
+                try {
+                    stm.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Administrateur read(String id) {
+        // TODO Auto-generated method stub
+        PreparedStatement stm = null;
+        try {
+//            Statement stm = cnx.createStatement();
+//            ResultSet r = stm.executeQuery("SELECT * FROM user WHERE numId = '" + id + "'");
+            //Avec requête paramétrée :
+            stm = cnx.prepareStatement("SELECT * FROM user WHERE codeAdministrateur = ?");
+            stm.setString(1,id);
+            ResultSet r = stm.executeQuery();
+            if (r.next()) {
+                //User c = new User(r.getString("numId"),r.getString("mdp"));
+                Administrateur c = new Administrateur();
+                c.setCodeAdministrateur(r.getString("courriel"));
+                c.setMotPasseAdministrateur(r.getString("motPasseAcheteur"));
+                r.close();
+                stm.close();
+                return c;
+            }
+        } catch (SQLException exp) {
+        } finally {
+            if (stm != null) {
+                try {
+                    stm.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+    @Override
+    public boolean update(Administrateur x) {
+        // TODO Auto-generated method stub
+        Statement stm = null;
+        try {
+            String req;
+            req = "UPDATE user SET motPasseAdministrateur = '" + x.getMotPasseAdministrateur() + "'"
+                    + " WHERE codeAdministrateur = '" + x.getCodeAdministrateur() + "'";
+            //System.out.println("REQUETE "+req);
+            stm = cnx.createStatement();
+            int n = stm.executeUpdate(req);
+            if (n > 0) {
+                stm.close();
+                return true;
+            }
+        } catch (SQLException exp) {
+        } finally {
+            if (stm != null) {
+                try {
+                    stm.close();
+                } catch (SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public List<Administrateur> findAll() {
+        // TODO Auto-generated method stub
+        List<Administrateur> liste = new LinkedList<Administrateur>();
+        try {
+            Statement stm = cnx.createStatement();
+            ResultSet r = stm.executeQuery("SELECT * FROM administrateur");
+            while (r.next()) {
+                Administrateur c = new Administrateur(r.getString("codeAdministrateur"),
+                        r.getString("motPasseAdministrateur"));
+                liste.add(c);
+            }
+            r.close();
+            stm.close();
+        } catch (SQLException exp) {
+        }
+        return liste;
+    }
+}
